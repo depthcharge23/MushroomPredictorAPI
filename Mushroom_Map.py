02e67258cf5a08eacfa7d23ef33b25c6e6a7e316
depthcharge23 <aamathews23@gmail.com>
2020-05-06 22:17:51 -0700

    Created function for generating data visuals

---
+++ Mushroom_Map.py
@@ -0,0 +1,195 @@
+class Mushroom_Map:
+    data = {
+        'cap-shape': {
+            0: ['Bell', 'Conical', 'Convex', 'Flat', 'Knobbed', 'Sunken'],
+            1: 'Bell',
+            2: 'Conical',
+            3: 'Convex',
+            4: 'Flat',
+            5: 'Knobbed',
+            6: 'Sunken'
+        },
+        'cap-surface': {
+            0: ['Fibrous', 'Grooves', 'Scaly', 'Smooth'],
+            1: 'Fibrous',
+            2: 'Grooves',
+            3: 'Scaly',
+            4: 'Smooth'
+        },
+        'cap-color': {
+            0: ['Brown', 'Buff', 'Cinnamon', 'Gray', 'Green', 'Pink', 'Purple', 'Red', 'White', 'Yellow'],
+            1: 'Brown',
+            2: 'Buff',
+            3: 'Cinnamon',
+            4: 'Gray',
+            5: 'Green',
+            6: 'Pink',
+            7: 'Purple',
+            8: 'Red',
+            9: 'White',
+            10: 'Yellow'
+        },
+        'bruises': {
+            0: ['Yes', 'No'],
+            1: 'Yes',
+            2: 'No'
+        },
+        'odor': {
+            0: ['Almond', 'Anise', 'Creosote', 'Fishy', 'Foul', 'Musty', 'Pungent', 'Spicy', 'None'],
+            1: 'Almond',
+            2: 'Anise',
+            3: 'Creosote',
+            4: 'Fishy',
+            5: 'Foul',
+            6: 'Musty',
+            7: 'Pungent',
+            8: 'Spicy',
+            9: 'None'
+        },
+        'gill-attachment': {
+            0: ['Attached', 'Descending', 'Free', 'Notched'],
+            1: 'Attached',
+            2: 'Descending',
+            3: 'Free',
+            4: 'Notched'
+        },
+        'gill-spacing': {
+            0: ['Close', 'Crowded', 'Distant'],
+            1: 'Close',
+            2: 'Crowded',
+            3: 'Distant'
+        },
+        'gill-size': {
+            0: ['Broad', 'Narrow'],
+            1: 'Broad',
+            2: 'Narrow'
+        },
+        'gill-color': {
+            0: ['Black', 'Brown', 'Buff', 'Chocolate', 'Gray', 'Green', 'Orange', 'Pink', 'Purple', 'Red', 'White', 'Yellow'],
+            1: 'Black',
+            2: 'Brown',
+            3: 'Buff',
+            4: 'Chocolate',
+            5: 'Gray',
+            6: 'Green',
+            7: 'Orange',
+            8: 'Pink',
+            9: 'Purple',
+            10: 'Red',
+            11: 'White',
+            12: 'Yellow'
+        },
+        'stalk-shape': {
+            0: ['Enlarging', 'Tapering'],
+            1: 'Enlarging',
+            2: 'Tapering'
+        },
+        'stalk-root': {
+            0: ['Bulbous', 'Club', 'Cup', 'Equal', 'Rhizomorphs', 'Rooted', 'Missing'],
+            1: 'Bulbous',
+            2: 'Club',
+            3: 'Cup',
+            4: 'Equal',
+            5: 'Rhizomorphs',
+            6: 'Rooted',
+            7: 'Missing'
+        },
+        'stalk-surface-above-ring': {
+            0: ['Fibrous', 'Scaly', 'Silky', 'Smooth'],
+            1: 'Fibrous',
+            2: 'Scaly',
+            3: 'Silky',
+            4: 'Smooth'
+        },
+        'stalk-surface-below-ring': {
+            0: ['Fibrous', 'Scaly', 'Silky', 'Smooth'],
+            1: 'Fibrous',
+            2: 'Scaly',
+            3: 'Silky',
+            4: 'Smooth'
+        },
+        'stalk-color-above-ring': {
+            0: ['Brown', 'Buff', 'Cinnamon', 'Gray', 'Orange', 'Pink', 'Red', 'White', 'Yellow'],
+            1: 'Brown',
+            2: 'Buff',
+            3: 'Cinnamon',
+            4: 'Gray',
+            5: 'Orange',
+            6: 'Pink',
+            7: 'Red',
+            8: 'White',
+            9: 'Yellow'
+        },
+        'stalk-color-below-ring': {
+            0: ['Brown', 'Buff', 'Cinnamon', 'Gray', 'Orange', 'Pink', 'Red', 'White', 'Yellow'],
+            1: 'Brown',
+            2: 'Buff',
+            3: 'Cinnamon',
+            4: 'Gray',
+            5: 'Orange',
+            6: 'Pink',
+            7: 'Red',
+            8: 'White',
+            9: 'Yellow'
+        },
+        'veil-type': {
+            0: ['Partial', 'Universal'],
+            1: 'Partial',
+            2: 'Universal'
+        },
+        'veil-color': {
+            0: ['Brown', 'Orange', 'White', 'Yellow'],
+            1: 'Brown',
+            2: 'Orange',
+            3: 'White',
+            4: 'Yellow'
+        },
+        'ring-number': {
+            0: ['One', 'Two', 'None'],
+            1: 'One',
+            2: 'Two',
+            3: 'None'
+        },
+        'ring-type': {
+            0: ['Cobwebby', 'Evanescent', 'Flaring', 'Large', 'Pendant', 'Sheathing', 'Zone', 'None'],
+            1: 'Cobwebby',
+            2: 'Evanescent',
+            3: 'Flaring',
+            4: 'Large',
+            5: 'Pendant',
+            6: 'Sheathing',
+            7: 'Zone',
+            8: 'None'
+        },
+        'spore-print-color': {
+            0: ['Black', 'Brown', 'Buff', 'Chocolate', 'Green', 'Orange', 'Purple', 'White', 'Yellow'],
+            1: 'Black',
+            2: 'Brown',
+            3: 'Buff',
+            4: 'Chocolate',
+            5: 'Green',
+            6: 'Orange',
+            7: 'Purple',
+            8: 'White',
+            9: 'Yellow'
+        },
+        'population': {
+            0: ['Abundant', 'Clustered', 'Numerous', 'Scattered', 'Several', 'Solitary'],
+            1: 'Abundant',
+            2: 'Clustered',
+            3: 'Numerous',
+            4: 'Scattered',
+            5: 'Several',
+            6: 'Solitary'
+        },
+        'habitat': {
+            0: ['Grasses', 'Leaves', 'Meadows', 'Paths', 'Urban', 'Waste', 'Woods'],
+            1: 'Grasses',
+            2: 'Leaves',
+            3: 'Meadows',
+            4: 'Paths',
+            5: 'Urban',
+            6: 'Waste',
+            7: 'Woods'
+        }
+    }
--- Mushroom_SVM.py
+++ Mushroom_SVM.py
@@ -6,6 +6,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 from io import BytesIO
 import base64
+from Mushroom_Map import Mushroom_Map
 
 uri = "mongodb://mushroom-predictor:ZkoVuLdDDPS0gwI8zn4zMgjFRqMZyKKTwfYs94LOPcAnP69VB2kWjyE7OxXns4omwzbIreZPgtX8KmNrNde4JQ==@mushroom-predictor.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@mushroom-predictor@&retrywrites=false"
 client = MongoClient(uri, ssl=True)
@@ -34,28 +35,74 @@ class Mushroom_SVM:
 
     def predict(self, sample):
         return self.clf.predict(np.array([sample]))
-    
-    def get_graph(self, graph_type='basic'):
+
+    def get_graph(self, graph_type='basic', prop=''):
         mushrooms = list(mushroom_db.find())
         df = pd.DataFrame(mushrooms)
+        mm = Mushroom_Map().data
+
+        fig = plt.figure(figsize=(5, 4))
+        fig_data = BytesIO()
 
         if (graph_type == 'basic'):
-            # Basic graph calculations and assignements
             categories = ['Poisonous', 'Edible']
+
             poisonous_freq = len(df[df['class'] == 1]) / len(df) * 100
             edible_freq = len(df[df['class'] == 2]) / len(df) * 100
-            frequencies = [poisonous_freq, edible_freq]
 
-            # Creation of the graph
-            fig = plt.figure(figsize=(4,5))
+            fig = plt.figure(figsize=(5, 4))
 
-            plt.pie(frequencies, autopct='%0.2f%%')
+            freqs = [poisonous_freq, edible_freq]
+
+            plt.pie(freqs, autopct='%0.2f%%')
             plt.axis('equal')
             plt.legend(categories)
             plt.title('Edible VS Poisonous')
 
-            # Saving the graph to a stream of bytes
-            fig_data = BytesIO()
             fig.savefig(fig_data, format='png')
 
-            return base64.b64encode(fig_data.getvalue()).decode('utf-8').replace('\n', '')
+        elif (graph_type == 'pie'):
+            if prop != '':
+                prop_map = mm[prop]
+                column_vals = sorted(df[prop].unique())
+
+                freqs = []
+                x_labels = prop_map[0]
+
+                for val in column_vals:
+                    freqs.append(len(df[df[prop] == val]) / len(df) * 100)
+
+                plt.figure(figsize=(5, 4))
+                plt.pie(freqs, autopct='%0.2f%%')
+                plt.axis('equal')
+                plt.legend(x_labels)
+                plt.title(prop.capitalize())
+
+                fig.save(fig_data, format='png')
+
+        elif (graph_type == 'bar'):
+            if prop != '':
+                prop_map = mm[prop]
+
+                x_vals = sorted(df[prop].unique())
+                y_vals = []
+
+                x_labels = prop_map[0]
+
+                for val in x_vals:
+                    y_vals.append(len(df[df[prop] == val]))
+
+                plt.bar(range(len(x_vals)), y_vals)
+
+                ax = plt.subplot()
+                ax.set_xticks(range(len(x_vals)))
+                ax.set_xticklabels(x_labels)
+                ax.set_xlabel(prop.capitalize() + ' Types')
+                ax.set_ylabel('Amount of Mushrooms')
+
+                plt.title(prop.capitalize())
+
+                fig.save(fig_data, format='png')
+        
+        return base64.b64encode(fig_data.getvalue()).decode('utf-8').replace('\n', '')
+        
