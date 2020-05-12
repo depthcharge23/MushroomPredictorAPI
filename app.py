c2e344e729e40d966b6b626dc589b515b94b6966
depthcharge23 <aamathews23@gmail.com>
2020-05-11 19:41:03 -0700

    Adding user log in route

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 from flask import Flask, jsonify, request, send_file
 from Mushroom_SVM import Mushroom_SVM
+import pyodbc
+
+server = 'mushroom-predictor.database.windows.net'
+database = 'mushroom-predictor'
+username = 'ammathews'
+password = '22ilbbid!4274'
+driver = '{ODBC Driver 17 for SQL Server}'
+cnxn = pyodbc.connect('DRIVER=' + driver + ';SERVER=' + server + ';PORT=1433;DATABASE=' + database + ';UID=' + username + ';PWD=' + password + ';TRUSTED_CONNECTION=yes;')
+cursor = cnxn.cursor()
 
 svm = Mushroom_SVM()
 svm.train()
@@ -27,3 +36,17 @@ def get_graph():
     return jsonify({
         'image': image
     })
+
+@app.route('/get-user', methods=['POST'])
+def get_user():
+    username = request.get_json()['username']
+    password = request.get_json()['password']
+
+    try:
+        cursor.execute('SELECT COUNT(*) AS row FROM dbo.USERS u WHERE u.USER_NAME = ' + username + ' AND u.PASSWORD = ' + password)
+        data = cursor.fetchone()
+        print(data)
+    except:
+        print('error')
+
+    return 'nice'
